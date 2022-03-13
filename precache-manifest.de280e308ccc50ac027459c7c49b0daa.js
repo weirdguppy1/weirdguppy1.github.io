ecea1812e06ab7858a172785e835dab237bc0036
Mark Fang <mark.fang.mo@gmail.com>
2022-03-12 21:58:27 -0600

    Updates

---
+++ precache-manifest.de280e308ccc50ac027459c7c49b0daa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7efb6a0c62c815193ad311fb32f3a232",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb372bdc0dd21a8d0ef6",
+    "url": "/static/css/main.d6de8c59.chunk.css"
+  },
+  {
+    "revision": "0af8604f09a29f9f0c89",
+    "url": "/static/js/2.bbe34d50.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.bbe34d50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb372bdc0dd21a8d0ef6",
+    "url": "/static/js/main.ac8f7122.chunk.js"
+  },
+  {
+    "revision": "8997fcea3f2ba08b753e",
+    "url": "/static/js/runtime-main.732b0f4d.js"
+  },
+  {
+    "revision": "44d3816cd75137b2694ca444e9b92798",
+    "url": "/static/media/IMG_1945.44d3816c.jpg"
+  },
+  {
+    "revision": "00d40ab5df0114fcc25f3a743d46598f",
+    "url": "/static/media/checkinproject.00d40ab5.png"
+  },
+  {
+    "revision": "c7ce77a3c11e9ccbc42da0757ac2354d",
+    "url": "/static/media/mybio.c7ce77a3.png"
+  },
+  {
+    "revision": "c6d7c7a4006c57122dd0edb070b7242f",
+    "url": "/static/media/personalwebsite.c6d7c7a4.png"
+  },
+  {
+    "revision": "0067bc6b417e881f6c571b2e179f9fdd",
+    "url": "/static/media/plumapp.0067bc6b.png"
+  },
+  {
+    "revision": "700591cc6b76bb95b6575e6b0e9e0ab2",
+    "url": "/static/media/quotesapp.700591cc.png"
+  }
+]);
